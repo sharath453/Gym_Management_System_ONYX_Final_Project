d0fdb176a2458bb97ec17360e3e08a3b1688a4dd
Koush28 <koushik.a.r8055@gmail.com>
2025-10-03 23:39:43 +0530

    added models and serializers

--- backend/member/admin.py
+++ backend/member/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from .models import Member
+from .models import Member, BMIHistory
 
 @admin.register(Member)
 class MemberAdmin(admin.ModelAdmin):
-    list_display = ('username', 'name', 'email', 'gender', 'join_date', 'plan')
+    list_display = ('username', 'name', 'email', 'gender', 'join_date', 'plan', 'trainer', 'height', 'weight')
+    search_fields = ('username', 'name', 'email')
+    list_filter = ('gender', 'plan', 'trainer')
+
+
+@admin.register(BMIHistory)
+class BMIHistoryAdmin(admin.ModelAdmin):
+    list_display = ('member', 'bmi', 'weight', 'height', 'date')
+    list_filter = ('date', 'member')
--- backend/member/serializers.py
+++ backend/member/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
-from .models import Member
+from .models import Member, BMIHistory
 
 class MemberSerializer(serializers.ModelSerializer):
+    bmi = serializers.ReadOnlyField()  # so BMI gets included but not overwritten
+
     class Meta:
         model = Member
+        fields = '__all__'
+
+
+class BMIHistorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BMIHistory
         fields = '__all__'
