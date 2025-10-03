d66d84f4a64c263598c671193b1a21a4c9fa821c
Koush28 <koushik.a.r8055@gmail.com>
2025-10-03 15:25:08 +0530

    Member model: add height, weight, BMIHistory; add serializers

--- backend/member/serializers.py
+++ backend/member/serializers.py
@@ -1,7 +1,33 @@
 from rest_framework import serializers
-from .models import Member
+from .models import Member, BMIHistory
 
+# Serializer for BMI history
+class BMIHistorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BMIHistory
+        fields = ['bmi', 'weight', 'height', 'date']
+
+
+# Serializer for Member
 class MemberSerializer(serializers.ModelSerializer):
+    bmi_history = BMIHistorySerializer(many=True, read_only=True)  # nested serializer
+    bmi = serializers.FloatField(source='bmi', read_only=True)      # current BMI
+
     class Meta:
         model = Member
-        fields = '__all__'
+        fields = [
+            'id',
+            'username',
+            'name',
+            'email',
+            'gender',
+            'join_date',
+            'plan',
+            'trainer',
+            'height',
+            'weight',
+            'bmi',
+            'bmi_history',
+            'created_at',
+            'updated_at',
+        ]
