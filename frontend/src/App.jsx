8e62111157bec94530510a3a4a7d1d66a0f5219e
Karthik N P <npkarthik2003@gmail.com>
2025-10-04 23:10:43 +0530

    AdminMember page crud operations added and login page css modified

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,44 +1,48 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route,Navigate } from 'react-router-dom';
-import AdminLayout from './components/Admin/AdminLayout';
-import Dashboard from './pages/Admin/Dashboard';
-import Members from './pages/Admin/Members';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import AdminLayout from "./components/Admin/AdminLayout";
+import Dashboard from "./pages/Admin/Dashboard";
+import Members from "./pages/Admin/Members";
 // import Trainers from './pages/Admin/Trainers';
 // import Plans from './pages/Plans';
 // import Profile from './pages/Profile';
-import './App.css';
+import "./App.css";
 import Login from "./components/Admin/Login";
-import Dashboard from "./components/Admin/Dashboard";
 import ProtectedRoutes from "./components/ProtectRoutes/ProtectRoutes";
 
 function App() {
   return (
     <Router>
-      {/* <AdminLayout>
-        <Routes>
-        <Route
-          path="/admin/dashboard"
+      <Routes>
+        <Route path="/login" element={<Login />} />
+
+        {/* Protected Admin routes */}
+        <Route 
+          path="/admin" 
           element={
             <ProtectedRoutes role="Admin">
-              <Dashboard />
+              <AdminLayout />
             </ProtectedRoutes>
           }
-        />
-          <Route path="/admin/dashboard" element={<Dashboard />} />
-          <Route path="/admin/members" element={<Members />} />
-          <Route path="/admin/trainers" element={<Trainers />} />
-          <Route path="/admin/plans" element={<Plans />} />
-          <Route path="/admin/profile" element={<Profile />} />
-        </Routes>
-      </AdminLayout> */}
-        <Routes>
-        <Route path="" element={<Login />} />
-        <Route path="/login" element={<Login />} />
+        >
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="members" element={<Members />} />
+          {/* <Route path="trainers" element={<Trainers />} />
+          <Route path="plans" element={<Plans />} />
+          <Route path="profile" element={<Profile />} /> */}
+          <Route index element={<Navigate to="dashboard" />} />
+        </Route>
+
+        <Route path="/" element={<Navigate to="/login" />} />
         <Route path="*" element={<Navigate to="/login" />} />
-      </Routes>     
+      </Routes>
     </Router>
   );
 }
 
 export default App;
-
